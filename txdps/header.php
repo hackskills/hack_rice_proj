9baa624a6b8885b4b5c4a3030a11be9f61773791
Shaquille Wyan Que <shaquillewyanque@shaquilles-mbp.dyndns.rice.edu>
2018-09-14 18:34:57 -0500

    Added website navigation and website files

--- txdps/header.php
+++ txdps/header.php
@@ -32,7 +32,15 @@ session_start();
         <li class="nav-item">
             <a class="nav-link" href="index.php">Home</a>
         </li>
-        
+        <li class="nav-item">
+            <a class="nav-link" href="services.php">Services</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="employment.php">Employment</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="aboutUs.php">About Us</a>
+        </li>
     </ul>
 </div>
 </nav>

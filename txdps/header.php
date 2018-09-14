b268751682a10c0ef30c6c4a6610702d19a36b81
Mista Max <mmctheace@gmail.com>
2018-09-14 18:25:14 -0500

    added texas dps website code

---
+++ txdps/header.php
@@ -0,0 +1,38 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="main.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+    <!--<link rel="stylesheet" href="../style/style.css">-->
+    <title>TxDPS</title>
+    <link rel="shortcut icon" type="RRGaming_Logo.png" href="favicon.ico" />
+	<!--<link rel="stylesheet" type="text/css" href="style.css">-->
+</head>
+<body id="body-over">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand" href="index.php"><img src="RRGaming_Logo.png" id="logo-badge" width="50" height="50">Roadrunner Gaming</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+    aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+        </li>
+        
+    </ul>
+</div>
+</nav>
---
+++ txdps/index.php
@@ -0,0 +1,25 @@
+<?php
+	include_once 'header.php';
+?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="position-relative overflow-hidden p-12 p-md-12 m-md-12 text-center bg-semilight">
+                    <div class="col-md-12 p-lg-12 mx-auto my-12">
+                        <h1 class="display-4 font-weight-normal">Roadrunner Gaming</h1>
+                        <p class="lead font-weight-normal">We are a gaming club located in San Antonio, here to be exact</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <iframe width="100%" height="500" scrolling="no" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCUrq9SGaK6YIe6ZLYcT80Gun-JO6Ui9vk
+                          &q=Universite+of+Texas+at+San+Antonio" allowfullscreen>
+                </iframe>
+            </div>
+        </div>
+    </div>
+<?php
+	include_once 'footer.php';
+?>

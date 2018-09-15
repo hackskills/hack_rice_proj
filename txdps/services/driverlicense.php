5fb36895bbcf97863d1c75a784d9b262e2f863f0
Shaquille Wyan Que <shaquillewyanque@Shaquilles-MacBook-Pro.local>
2018-09-15 18:53:04 -0500

    Updated header to link driver license

--- txdps/services/driverlicense.php
+++ txdps/services/driverlicense.php
@@ -15,7 +15,7 @@
                             <div class="card bg-light">
                               <div class="card-header" id="headingOne">
                                 <h5 class="mb-0">
-                                  <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                  <button href="/services/driverlicense" class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                                     Driver License
                                   </button>
                                 </h5>

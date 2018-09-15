cf21927f80590109e70a74ee1aa8f4258a8cf337
Shaquille Wyan Que <shaquillewyanque@shaquilles-mbp.dyndns.rice.edu>
2018-09-14 22:33:24 -0500

    Added navigation bar

--- txdps/header.php
+++ txdps/header.php
@@ -22,18 +22,70 @@ session_start();
 <body id="body-over">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 <a class="navbar-brand" href="index.php"><img src="RRGaming_Logo.png" id="logo-badge" width="50" height="50">Roadrunner Gaming</a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
     aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
 </button>
 
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
+<div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+        <li class="nav-item active">
             <a class="nav-link" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="services.php">Services</a>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="services.php" id="navbarDropdownMenuLink" role="menu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Services 
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">
+                    Crime Records</a>
+                    <ul class="dropdown-submenu">
+                        <li><a class="dropdown-item" href="#">Criminal History Search</a></li>
+                        <li><a class="dropdown-item" href="#">Fingerprint Services</a></li>
+                        <li><a class="dropdown-item" href="#">Sex Offender Search</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">
+                    Criminal Investigations</a>
+                    <ul class="dropdown-submenu">
+                        <li><a class="dropdown-item" href="#">Polygraph School</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle: href="#">
+                    Driver License/ID Home</a>
+                    <ul class="dropdown-submenu">
+                        <li><a class="dropdown-item" href="#"></a>Address Change</li>
+                        <li><a class="dropdown-item" href="#">Driver Record Request</a></li>
+                        <li><a class="dropdown-item" href="#">Office Locations</a></li>
+                        <li><a class="dropdown-item" href="#">Renewal</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle: href="#">
+                    Law Enforcement Support</a>
+                    <ul class="dropdown-submenu">
+                        <li><a class="dropdown-item" href="#"></a>Breath Alcohol Testing</li>
+                        <li><a class="dropdown-item" href="#">Communications</a></li>
+                        <li><a class="dropdown-item" href="#">Crime Laboratory</a></li>
+                        <li><a class="dropdown-item" href="#">NIBRS/TIBRS</a></li>
+                    </ul>
+                </li>    
+                <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle: href="#">
+                    Regulatory Services</a>
+                    <ul class="dropdown-submenu">
+                        <li><a class="dropdown-item" href="#"></a>Capitol Access Pass</li>
+                        <li><a class="dropdown-item" href="#">Commissioned Online Prescription System</a></li>
+                        <li><a class="dropdown-item" href="#">Compassionate Use Program</a></li>
+                        <li><a class="dropdown-item" href="#">Handgun Licensing</a></li>
+                        <li><a class="dropdown-item" href="#"></a>Ignition Interlock</li>
+                        <li><a class="dropdown-item" href="#">Peyote</a></li>
+                        <li><a class="dropdown-item" href="#">Precursor Laboratory Apparatus</a></li>
+                        <li><a class="dropdown-item" href="#">Private Security</a></li>
+                        <li><a class="dropdown-item" href="#">Texas Metals Program</a></li>
+                        <li><a class="dropdown-item" href="#">Vehicle Inspection</a></li>
+                    </ul>
+                </li>
+                    <a class="dropdown-item" href="#">Law Enforcement Support</a>
+                    <a class="dropdown-item" href="#">Regulatory Services</a>                        <a class="dropdown-item" href="#">Texas LESO Program</a>
+            </ul>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="employment.php">Employment</a>

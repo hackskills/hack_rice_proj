735ce535f8aea1133fa39197dd4caf7cd04e0b45
Shaquille Wyan Que <shaquillewyanque@shaquilles-mbp.dyndns.rice.edu>
2018-09-15 16:29:43 -0500

    Updated driver license file. Still incomplete

--- txdps/services/driverlicense.php
+++ txdps/services/driverlicense.php
@@ -3,14 +3,15 @@
     <div class="row">
             <div class="col">
                 <div class="position-relative overflow-hidden p-12 p-md-12 m-md-12 text-center bg-semilight">
+                    <div class="card text-white bg-primary mb-3" style="padding:10px 0px 0px 0px">
                     <h1>Driver License Services</h1>
-                    <a href="#addresschange">Address Change</a>
-                    <a href="#driverrecordrequests">Driver Record Requests</a>
-                    <a href="#officelocations">Office Locations</a>
-                    <a href="#renewal">Renewal</a>
+                    <a href="#addresschange" style="color:white">Address Change</a>
+                    <a href="#driverrecordrequests" style="color:white">Driver Record Requests</a>
+                    <a href="#officelocations" style="color:white">Office Locations</a>
+                    <a href="#renewal" style="color:white">Renewal</a>
                     <h2 id="addresschange">Address Change</h2>
                     <div id="accordion">
-                        <div class="card">
+                        <div class="card bg-light">
                           <div class="card-header" id="headingOne">
                             <h5 class="mb-0">
                               <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
@@ -18,8 +19,9 @@
                               </button>
                             </h5>
                           </div>
-                          <div id="collapseOne" class="collapse show" style="text-align:left" aria-labelledby="headingOne" data-parent="#accordion">
-                            <div class="card-body" style="padding:8px 10% 4px 8%">
+                          <div id="collapseOne" class="collapse" style="text-align:left" aria-labelledby="headingOne" data-parent="#accordion">
+                            <div class="card-body" style="padding:8px 10% 4px">
+                                <div style="color:steelblue">
                                 You must meet the following requirements to change your address online:
                                 <ol>
                                     <li>Your driver license is not expired. If it is expired, you can only change your address if you also renew it at the same time.</li>
@@ -38,7 +40,9 @@
                                     <li>The last four digits of your Social Security Number.</li>
                                 </ol>
                             </div>
+                            </div>
                           </div>
+                        </div>
                         </div>
                         <div class="card">
                           <div class="card-header" id="headingTwo">
@@ -48,8 +52,9 @@
                               </button>
                             </h5>
                           </div>
-                          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-                            <div class="card-body" style="text-align:left">
+                          <div id="collapseTwo" class="collapse" style="text-align:left" aria-labelledby="headingTwo" data-parent="#accordion">
+                            <div class="card-body" style="padding:8px 10% 4px">
+                                <div style="color:steelblue">
                                 You must meet the following requirements to change your address online:
                                 <ol>
                                     <li>Your commercial driver license is not expired. If it is expired, you can only change your address at your local driver license office.</li>
@@ -59,6 +64,14 @@
                                     <li>Your Social Security Number is already on file.</li>
                                     <li>You are a U.S. citizen.</li>
                                 </ol>
+                                You must have <b>all</b> of the following items to use the online service:
+                                <ol>
+                                    <li>Your current commercial driver license;</li>
+                                    <li>A printer to print your temporary driver license and payment receipt–it will not be emailed to you;</li>
+                                    <li>A valid credit card: MasterCard, Visa, Discover or American Express only;</li>
+                                    <li>The last four digits of your Social Security Number.</li>
+                                </ol>
+                                </div>
                             </div>
                           </div>
                         </div>
@@ -66,13 +79,28 @@
                             <div class="card-header" id="headingThree">
                               <h5 class="mb-0">
                                 <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                  Collapsible Group Item #3
+                                  ID Card
                                 </button>
                               </h5>
                             </div>
-                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-                              <div class="card-body">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            <div id="collapseThree" class="collapse" style="text-align:left" aria-labelledby="headingThree" data-parent="#accordion">
+                              <div class="card-body" style="padding:8px 10% 4px">
+                                <div style="color:steelblue">
+                                You must meet the following requirements to change your address online:
+                                <ol>
+                                    <li>Your ID card is not expired. If it is expired, you can only change your address if you renew it at the same time.</li>
+                                    <li>You are at least 18 years of age.</li>
+                                    <li>Your Social Security Number is on file.</li>
+                                    <li>You are a U.S. citizen.</li>
+                                </ol>
+                                You must have <b>all</b> of the following items to use the online service:
+                                <ol>
+                                    <li>Your current ID card;</li>
+                                    <li>A printer to print your temporary driver license and payment receipt–it will not be emailed to you;</li>
+                                    <li>A valid credit card: MasterCard, Visa, Discover or American Express only;</li>
+                                    <li>The last four digits of your Social Security Number.</li>
+                                </ol>
+                            </div>
                               </div>
                             </div>
                           </div>

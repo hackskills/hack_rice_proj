1cb2b7f6bc69190f0cc0d72a680a09c085ab0f7c
Shaquille Wyan Que <shaquillewyanque@shaquilles-mbp.dyndns.rice.edu>
2018-09-15 14:55:30 -0500

    Added initial driver license file

---
+++ txdps/services/driverlicense.php
@@ -0,0 +1,88 @@
+<?php include_once 'header.php';?>
+<div class="container-fluid">
+    <div class="row">
+            <div class="col">
+                <div class="position-relative overflow-hidden p-12 p-md-12 m-md-12 text-center bg-semilight">
+                    <h1>Driver License Services</h1>
+                    <a href="#addresschange">Address Change</a>
+                    <a href="#driverrecordrequests">Driver Record Requests</a>
+                    <a href="#officelocations">Office Locations</a>
+                    <a href="#renewal">Renewal</a>
+                    <h2 id="addresschange">Address Change</h2>
+                    <div id="accordion">
+                        <div class="card">
+                          <div class="card-header" id="headingOne">
+                            <h5 class="mb-0">
+                              <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Driver License
+                              </button>
+                            </h5>
+                          </div>
+                          <div id="collapseOne" class="collapse show" style="text-align:left" aria-labelledby="headingOne" data-parent="#accordion">
+                            <div class="card-body" style="padding:8px 10% 4px 8%">
+                                You must meet the following requirements to change your address online:
+                                <ol>
+                                    <li>Your driver license is not expired. If it is expired, you can only change your address if you also renew it at the same time.</li>
+                                    <li>You have a Class C or CM driver license. You cannot use the online services to change your address on a Class A, Class B or commercial driver license.</li>
+                                    <li>You are at least 18 years of age and your driver license is not a provisional or learner license.</li>
+                                    <li>You are younger than 79 years of age.</li>
+                                    <li>Your driver license is valid (not suspended or revoked).</li>
+                                    <li>Your Social Security Number is already on file.</li>
+                                    <li>You are a U.S. citizen.</li>
+                                </ol>
+                                You must have <b>all</b> of the following items to use the online service:
+                                <ol>
+                                    <li>Your current driver license;</li>
+                                    <li>A printer to print your temporary driver license and payment receipt–it will not be emailed to you;</li>
+                                    <li>A valid credit card: MasterCard, Visa, Discover or American Express only;</li>
+                                    <li>The last four digits of your Social Security Number.</li>
+                                </ol>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="card">
+                          <div class="card-header" id="headingTwo">
+                            <h5 class="mb-0">
+                              <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                Commercial Driver License
+                              </button>
+                            </h5>
+                          </div>
+                          <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                            <div class="card-body" style="text-align:left">
+                                You must meet the following requirements to change your address online:
+                                <ol>
+                                    <li>Your commercial driver license is not expired. If it is expired, you can only change your address at your local driver license office.</li>
+                                    <li>You are at least 18 years of age and your driver license is not a provisional or learner license.</li>
+                                    <li>You are younger than 79 years of age.</li>
+                                    <li>Your driver license is valid (not suspended or revoked).</li>
+                                    <li>Your Social Security Number is already on file.</li>
+                                    <li>You are a U.S. citizen.</li>
+                                </ol>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="headingThree">
+                              <h5 class="mb-0">
+                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                  Collapsible Group Item #3
+                                </button>
+                              </h5>
+                            </div>
+                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                              <div class="card-body">
+                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                              </div>
+                            </div>
+                          </div>
+                        </div>                      
+                    <h2 id="driverrecordrequests">Driver Record Requests</h2>
+                    <h2 id="officelocations">Office Locations</h2>
+                    <h2 id="renewal">Renewal</h2>
+            </div>
+    </div></div></div>
+
+<?php
+    include_once 'footer.php';
+?>
